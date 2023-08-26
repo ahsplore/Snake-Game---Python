dbaaadc2ae7b933624aece42acd8940101283d2b
Anannya K <142819550+ahsplore@users.noreply.github.com>
2023-08-26 14:47:53 +0530

    Update scoreboard.py
    now the user can keep the track of their highest score too :)

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -9,6 +9,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open('data.txt') as f:
+            self.high_score = int(f.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -16,13 +18,21 @@ class ScoreBoard(Turtle):
         self.updated_scoreboard()
 
     def updated_scoreboard(self):
-        self.write(f'Score: {self.score}', align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.updated_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write('Game Over...', align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+        # self.goto(0, 0)
+        # self.write('Game Over...', align=ALIGNMENT, font=FONT)
+
+    def score_reset(self):
+    if self.score > self.high_score:
+        with open('data.txt', mode='w') as f:
+            f.write(str(self.score))
+        self.high_score = self.score
+    self.score = 0
+    self.updated_scoreboard()
